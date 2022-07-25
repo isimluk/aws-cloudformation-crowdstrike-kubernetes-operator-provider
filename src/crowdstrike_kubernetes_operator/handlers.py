af992e92ceb67e3d5b5bf1886c8eca25855b6695
Simon Lukasik <simon.lukasik@crowdstrike.com>
2022-07-25 11:18:04 +0100

    wip

--- src/crowdstrike_kubernetes_operator/handlers.py
+++ src/crowdstrike_kubernetes_operator/handlers.py
@@ -75,37 +75,6 @@ def create_handler(
     return progress
 
 
-def delme():
-    # TODO: put code here
-
-    # Example:
-    try:
-
-        # primary identifier from example
-        primary_identifier = None
-
-        # setting up random primary identifier compliant with cfn standard
-        if primary_identifier is None:
-            primary_identifier = identifier_utils.generate_resource_identifier(
-                stack_id_or_name=request.stackId,
-                logical_resource_id=request.logicalResourceIdentifier,
-                client_request_token=request.clientRequestToken,
-                max_length=255
-                )
-
-        if isinstance(session, SessionProxy):
-            client = session.client("s3")
-        # Setting Status to success will signal to cfn that the operation is complete
-        progress.status = OperationStatus.SUCCESS
-    except TypeError as e:
-        # exceptions module lets CloudFormation know the type of failure that occurred
-        raise exceptions.InternalFailure(f"was not expecting type {e}")
-        # this can also be done by returning a failed progress event
-        # return ProgressEvent.failed(HandlerErrorCode.InternalFailure, f"was not expecting type {e}")
-
-    return read_handler(session, request, callback_context)
-
-
 @resource.handler(Action.UPDATE)
 def update_handler(
     session: Optional[SessionProxy],
